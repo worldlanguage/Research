7a91984a5970e23477cd1406b5e09b6ffe6c078f
AXiu <45101190+worldlanguage@users.noreply.github.com>
2022-04-12 22:59:15 +0800

    Delete CV directory

--- CV/GNN-Re-Ranking/set_env.sh
+++
@@ -1,1 +0,0 @@
-export LD_LIBRARY_PATH=./lib:$( python -c 'import paddle; print(paddle.sysconfig.get_lib())'):$LD_LIBRARY_PATH
--- CV/SemSegPaddleDG/train.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# single_card:
-CUDA_VISIBLE_DEVICES=7 python3 train.py  --use_gpu \
-                                  --cfg ./configs/pspnet_res101_cityscapes.yaml 
-# multi_card:
-#python3  -m paddle.distributed.launch --selected_gpus=6,7 train.py \
-#                                  --use_gpu --cfg configs/pspnet_res101_cityscapes.yaml
-
